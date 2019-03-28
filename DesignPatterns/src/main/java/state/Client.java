563805a5acbfe7bd7bb9b9552019817365fdd040
wfm <wfm@erongdu.com>
2019-03-28 16:01:49 +0800

    添加了状态模式的示例代码

---
+++ DesignPatterns/src/main/java/state/Client.java
@@ -0,0 +1,10 @@
+package state;
+
+public class Client {
+    public static void main(String[] args) {
+        VoteManager voteManager = new VoteManager();
+        for (int i=0;i<9;i++){
+            voteManager.vote("admin","AAA");
+        }
+    }
+}
---
+++ DesignPatterns/src/main/java/state/NormalVoteState.java
@@ -0,0 +1,11 @@
+package state;
+
+/**
+ * 正常投票
+ */
+public class NormalVoteState implements VoteState {
+    @Override
+    public void vote(String user, String voteItem, VoteManager voteManager) {
+
+    }
+}
