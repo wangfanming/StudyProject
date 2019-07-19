ceb34a5c03a8584bc194845e984e323d03a68c80
wangfanming <1422590983@qq.com>
2019-07-20 00:15:13 +0800

    添加了部分注释

--- simple-rpc/src/main/java/com/wfm/rpcClientBasedNetty/future/RpcFuture.java
+++ simple-rpc/src/main/java/com/wfm/rpcClientBasedNetty/future/RpcFuture.java
@@ -6,13 +6,18 @@ import java.util.concurrent.TimeUnit;
 /**
  * @Auther: wangfanming
  * @Date: 2019/7/17 10:48
- * @Description:
+ * @Description: 异步方式下会立即返回一个Future,可以通过调用isDone()方法判断结果是否已经返回，通过get()获取返回结果，也可以通过setRpcFutureListener()
+ * 设置结果返回时的回调借口。
  */
 public class RpcFuture {
     public final static int STATE_AWAIT = 0;
     public final static int STATE_SUCCESS = 1;
     public final static int STATE_EXCEPTION = 2;
 
+    /*
+       RpcFuture内部使用了CountDownLatch,初始值为1。只有在调用setResult()方法(正常返回了远程调用结果)或调用setThrowable()方法(调用过程中有异常发生)后，
+       CountLatch才会减到0。在此之前，若设置了Listener的，将获得回调，若对RpcFuture对象调用get()方法将阻塞。
+     */
     private CountDownLatch countDownLatch;
     private Object result;
     private Throwable throwable;
@@ -26,7 +31,7 @@ public class RpcFuture {
 
     /**
      *
-     * 功能描述: 异步调用
+     * 功能描述:
      * @param:
      * @return:
      * @auther: wangfanming
@@ -50,7 +55,7 @@ public class RpcFuture {
 
     /**
      *
-     * 功能描述: 同步调用
+     * 功能描述:
      * @param:
      * @return:
      * @auther: wangfanming
